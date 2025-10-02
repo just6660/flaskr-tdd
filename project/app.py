17724c49f5e8d2fac8186ba5e0622f545465a1b6
Justin Chow <just6660@gmail.com>
2025-10-02 00:13:33 -0400

    postgresql

--- project/app.py
+++ project/app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from pathlib import Path
+import os
 
 from flask import (
     Flask,
@@ -26,6 +27,13 @@ PASSWORD = "admin"
 SECRET_KEY = "change_me"
 SQLALCHEMY_DATABASE_URI = f"sqlite:///{Path(basedir).joinpath(DATABASE)}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+url = os.getenv("DATABASE_URL", f"sqlite:///{Path(basedir).joinpath(DATABASE)}")
+
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = url
 
 
 # create and initialize a new Flask app
